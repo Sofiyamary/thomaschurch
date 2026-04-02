d661d4123ff3ab6edb51b63d1c6c4bf2b4bf5cd1
Sofiyamary <sofiyamaryd2001@gmail.com>
2026-04-02 12:38:08 +0530

    fix build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/stthomaschurch/',  
+  base: '/',  
   plugins: [tailwindcss()],
 })
